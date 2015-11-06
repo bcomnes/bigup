e58024cbc54107c11d035c0baac9fd47ea11d386
Bret Comnes <bcomnes@jaguarlandrover.com>
2015-11-05 17:07:19 -0800

    More stuff

--- cli.js
+++ cli.js
@@ -2,20 +2,24 @@
 
 var minimist = require('minimist')
 var argv = minimist(process.argv.slice(2))
-var bigup = require('./')
 var command = argv._.shift()
+
+var bigup = require('./')
 
 var commands = {
   init: bigup
 }
 
 if (commands.hasOwnProperty(command)) {
-  commands[command]()
+  commands[command](argv)
 } else {
   printHelp()
 }
 
 function printHelp () {
-  var helpString = ['usage: bigup [options]'].join('\n')
+  var helpString = [
+    'usage: bigup [options]',
+    '    init    initialize a new big presentation in the current working directory'
+  ].join('\n')
   console.log(helpString)
 }
--- index.js
+++ index.js
@@ -2,51 +2,103 @@ var path = require('path')
 var cp = require('child_process')
 var fs = require('fs')
 var npmWhoami = require('npm-whoami')
+var cp = require('cp')
+var bigPath = require.resolve('big')
+var parallel = require('run-parallel')
 
-function genPackageObj (options) {
-  var packageObj = {
+function bigup (options) {
+  console.log(options)
+}
+
+function getPackageObj (options) {
+  function packageObjFn () {
+  var package = {
     name: path.dirname(options.path),
     description: '',
     version: '1.0.0',
     author: options.name,
     devDependencies: {
-      'browserify': '*',
-      'big': 'github:tmcw/big#v1.0.1',
-      'budo': '*'
+      'big': 'github:tmcw/big#v1.0.1'
     },
     scripts: {
       start: 'budo index.js:bundle.js --dir static --live --open'
     }
   }
-  return packageObj
+ }
+  return packageObjFn
 }
 
-function npmInstall (dir, cb) {
-  cp.exec('npm i', {cww: dir}, cb)
+function npmInstallDev (dir, cb) {
+  var devDependencies = ['budo', 'browserify', 'github:tmcw/big#v1.0.1'].join(' ')
+  cp.exec('npm i budo ' + devDependencies + ' --save-dev', {cwd: dir}, cb)
+}
+
+var files = [
+  // an array of fileObj's
+  {
+    name: 'package.json',
+    readStream: strum(JSON.stringify(genPackageObj(options)))
+    dir: '.'
+  },
+  {
+    name: 'big.css')
+    readStream: fs.createReadStream(path.join(bigPath, this.name)),
+    dir: 'static'
+  },
+  {
+    name: 'index.html',
+    readStream: fs.createReadStream(path.join(__dirname, this.name)),
+    dir: 'static'
+  }
+]
+
+function allClear (files, cb) {
+  var statFiles = files.map(function (fileObj) {
+    return statCheck.bind(null, fileObj)
+  })
+  parallel(statFiles, cb)
+}
+
+function statCheck (fileObj, cb) {
+  var filePath = path.join(fileObj.dest)
+  fs.stat(fileObj.path, function (err, stats) {
+    if (stats) return cb(new Error(fileObj.path + ' already exists'))
+    cb(null) // file doesn't exist.. good to go
+  })
 }
 
 function savePackageObj (packageObj, dir, cb) {
   var packageJson = JSON.stringify(packageObj)
-  fs.writeFile(path.join(dir, 'pacakage.json'), packageJson, cb)
+  var packageJsonPath = path.join(dir, 'pacakage.json')
+  fs.stat(packageJsonPath, function(err, stats) {
+    if (stats) return cb(new Error(packageJsonPath + ' already exists!'))
+    fs.writeFile(packageJsonPath, packageJson, cb)
+  })
 }
 
 function saveCss (dir, cb) {
   var cssPath = path.join(require.resolve('big'), 'big.css')
   var destCss = path.join(dir, 'static', 'big.css')
-  var source = fs.createReadStream(cssPath)
-  var dest = fs.createWriteStream(destCss)
-  source.on('error', cb)
-  source.on('finish', cb)
-  source.pipe(dest)
+  fs.stat(packageJsonPath, function(err, stats) {
+    if (stats) return cb(new Error(destCss + ' already exists!'))
+    cp(cssPath, destCss, cb)
+  })
 }
 
 function saveHtml (dir, cb) {
-  var htmlPath = path.join(__dirname, index.html)
+  var htmlPath = path.join(__dirname, 'index.html')
+  var destHtml = path.join(dir, 'static', 'index.html')
+  fs.stat(packageJsonPath, function(err, stats) {
+    if (stats) return cb(new Error(destHtml + ' already exists!'))
+    cp(htmlPath, destHtml, cb)
+  })
 }
 
 function getNpmName (cb) {
-  name = npmWhoami(function normalize (err, name) {
+  var name = npmWhoami(function normalize (err, name) {
     if (err) return ''
     return name
   })
 }
+
+module.exports = bigup
